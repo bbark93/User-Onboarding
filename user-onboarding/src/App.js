746df463d9577e42670607fe70fd12dcf6cfd427
Brandon Barksdale <bbark93@gmail.com>
2023-07-04 12:42:34 -0400

    Made post request and displayed returned data to screen

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
 import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
 import Form from './Components/Form';
 import * as yup from 'yup'
 import schema from './validation/formSchema';
@@ -20,10 +21,16 @@ const initialFormErrors = {
 
 function App() {
   const [formValues, setFormValues] = useState(initialFormValues);
-  const [formErrors, setFormErrors] = useState(initialFormErrors)
+  const [formErrors, setFormErrors] = useState(initialFormErrors);
+  const [users, setUsers] = useState([])
 
   const handleSubmit = () => {
-    //WIP
+    axios.post('https://reqres.in/api/users', formValues)
+      .then(res => {
+        setUsers([ res.data, ...users ])
+      })
+      .catch(err => console.error(err))
+      .finally(() => setFormValues(initialFormValues));
   }
 
   const validate = (name, value) => {
@@ -40,7 +47,13 @@ function App() {
 
   return (
     <div className="App">
-      <Form values={formValues} change={handleChange} errors={formErrors} />
+      <Form values={formValues} change={handleChange} errors={formErrors} submit={handleSubmit} />
+      {users.map(user => (
+        <div key={user.id}>
+          <p>{user.createdAt}</p>
+          <p>{user.email}</p>
+        </div>
+      ))}
     </div>
   );
 }
