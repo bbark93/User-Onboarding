16538f94706a67fae675fb994e96e4dd1b651715
Brandon Barksdale <bbark93@gmail.com>
2023-07-03 20:28:35 -0400

    implemented form validation and error messaging

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import './App.css';
 import Form from './Components/Form';
+import * as yup from 'yup'
+import schema from './validation/formSchema';
 
 const initialFormValues = {
   username: '',
@@ -9,20 +11,36 @@ const initialFormValues = {
   tos: false
 }
 
+const initialFormErrors = {
+  username: '',
+  email: '',
+  password: '',
+  tos: ''
+}
+
 function App() {
   const [formValues, setFormValues] = useState(initialFormValues);
+  const [formErrors, setFormErrors] = useState(initialFormErrors)
 
   const handleSubmit = () => {
     //WIP
   }
 
+  const validate = (name, value) => {
+    yup.reach(schema, name)
+      .validate(value)
+      .then(() => setFormErrors({ ...formErrors, [name]: '' }))
+      .catch(err => setFormErrors({ ...formErrors, [name]: err.errors[0] }))
+  }
+
   const handleChange = (name, value) => {
+    validate(name,value);
     setFormValues({...formValues, [name]: value});
   }
 
   return (
     <div className="App">
-      <Form values={formValues} change={handleChange} />
+      <Form values={formValues} change={handleChange} errors={formErrors} />
     </div>
   );
 }
