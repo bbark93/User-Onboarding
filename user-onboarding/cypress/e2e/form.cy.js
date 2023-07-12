b9f18733d8720e86482cb91e69033c1ea15636b2
Brandon Barksdale <bbark93@gmail.com>
2023-07-11 23:03:39 -0400

    task 2

--- user-onboarding/cypress/e2e/form.cy.js
+++ user-onboarding/cypress/e2e/form.cy.js
@@ -17,6 +17,46 @@ describe('Form App', () => {
         submitBtn().should('exist')
         TOSCheckBx().should('exist')
         cy.contains(/create a friend!/i).should('exist')
-      })
+        cy.get(`input[type="checkbox"]`).should('not.be.checked')
+    })
+
+    describe('Filling out the inputs and submit to DOM', () => {
+        // We use optional "describe" blocks to organize and group our tests.
+        it('can navigate to the site', () => {
+          cy.url().should('include', 'localhost')
+        })
+    
+        it('can type in the inputs', () => {
+            nameInput()
+                .should('have.value', '')
+                .type('BBark93')
+                .should('have.value', 'BBark93')
+    
+            emailInput()
+                .should('have.value', '')
+                .type('bbark93@gmail.com')
+                .should('have.value', 'bbark93@gmail.com')
+
+            pwInput()
+                .should('have.value', '')
+                .type('password')
+                .should('have.value', 'password')
+        })
+
+        it('can check tos checkbox', () => {
+            cy.get(`input[type="checkbox"]`)
+                .check()
+                .should('be.checked')
+        })
+
+        it('can submit inputs and input appears on DOM', () => {
+            nameInput().type('BBark93')
+            emailInput().type('bbark93@gmail.com')
+            pwInput().type('password')
+            submitBtn().click()
+            cy.contains(/bbark93@gmail.com/).should('exist')
+        })
+    })
+
 
 })
